3b7e05ffa52f7e22db19c978735028da147fb8dc
Soham Zemse <sohamzemse143@gmail.com>
2019-06-10 11:44:53 +0530

    add isDrawPossible and move NewBetting event in the parent contract

--- contracts/BetDeEx.sol
+++ contracts/BetDeEx.sol
@@ -18,6 +18,13 @@ contract BetDeEx {
         string _description
     );
 
+    event NewBetting (
+        address indexed _betAddress,
+        address indexed _bettorAddress,
+        uint8 indexed _choice,
+        uint256 _bettorIndex
+    );
+
     event EndBetContract (
         address indexed _ender,
         address indexed _contractAddress,
@@ -50,8 +57,8 @@ contract BetDeEx {
 
     // bet functions
 
-    function createBet(string memory _description, uint8 _category, uint8 _subCategory, uint256 _minimumBet, uint256 _pricePercent) public onlyManager returns (address) {
-        Bet _newBet = new Bet(_description, _category, _subCategory, _minimumBet, _pricePercent);
+    function createBet(string memory _description, uint8 _category, uint8 _subCategory, uint256 _minimumBet, uint256 _pricePercent, bool _isDrawPossible) public onlyManager returns (address) {
+        Bet _newBet = new Bet(_description, _category, _subCategory, _minimumBet, _pricePercent, _isDrawPossible);
         bets.push(address(_newBet));
         isBetValid[address(_newBet)] = true;
 
@@ -63,6 +70,10 @@ contract BetDeEx {
     function addAmountToBet(uint256 _betAmountInExaEs) public onlyBetContract returns (bool) {
         betBalanceInExaEs[msg.sender] += _betAmountInExaEs;
         return true;
+    }
+
+    function emitNewBettingEvent(address _bettorAddress, uint8 _choice, uint256 _bettorIndex) public onlyBetContract {
+        emit NewBetting(msg.sender, _bettorAddress, _choice, _bettorIndex);
     }
 
     function emitEndEvent(address _ender, uint8 _result, uint256 _gasFee) public onlyBetContract {
@@ -118,6 +129,13 @@ contract Bet {
 
     BetDeEx betDeEx;
 
+    string public description;
+    bool isDrawPossible;
+    uint8 public category;
+    uint8 public subCategory;
+
+    uint8 public finalResult;
+
     address public endedBy;
 
     uint256 public creationBlockNumber;
@@ -127,22 +145,11 @@ contract Bet {
     uint256 public pricePercentPerThousand; // 1/1000
     uint256[3] public totalBetTokensInExaEsByChoice = [0, 0, 0]; // array of total bet value of no, yes, draw voters
 
-    string public description;
-    uint8 public category;
-    uint8 public subCategory;
 
-    uint8 public finalResult;
 
     Bettor[] public noBettors;
     Bettor[] public yesBettors;
     Bettor[] public drawBettors;
-
-    event NewBetting (
-        address _bettorAddress,
-        uint256 _betAmountInExaEs,
-        uint8 _choice,
-        uint256 _bettorsIndex
-    );
 
     modifier onlyManager() {
         require(betDeEx.isManager(msg.sender));
@@ -150,8 +157,9 @@ contract Bet {
     }
 
     // _pricePercentPerThousand is an integer from 0 to 1000. if we want to keep .2% then its value is 998.
-    constructor(string memory _description, uint8 _category, uint8 _subCategory, uint256 _minimumBetInExaEs, uint256 _pricePercentPerThousand) public {
+    constructor(string memory _description, uint8 _category, uint8 _subCategory, uint256 _minimumBetInExaEs, uint256 _pricePercentPerThousand, bool _isDrawPossible) public {
         description = _description;
+        isDrawPossible = _isDrawPossible;
         category = _category;
         subCategory = _subCategory;
         minimumBetInExaEs = _minimumBetInExaEs;
@@ -168,28 +176,28 @@ contract Bet {
     function enterBet(uint8 _choice, uint256 _betTokensInExaEs) public {
         require(_betTokensInExaEs >= minimumBetInExaEs);
         require(betDeEx.getBettorBalance(msg.sender) >= _betTokensInExaEs);
-        uint256 _id;
+        uint256 _arrayIndex;
         if (_choice == 0) {
             totalBetTokensInExaEsByChoice[0] += _betTokensInExaEs;
+            _arrayIndex = noBettors.length;
             noBettors.push(Bettor({
                 bettorAddress: msg.sender,
                 betAmountInExaEs: _betTokensInExaEs
             }));
-            _id = noBettors.length;
         } else if (_choice == 1) {
             totalBetTokensInExaEsByChoice[1] += _betTokensInExaEs;
+            _arrayIndex = yesBettors.length;
             yesBettors.push(Bettor({
                 bettorAddress: msg.sender,
                 betAmountInExaEs: _betTokensInExaEs
             }));
-            _id = yesBettors.length;
         } else if (_choice == 2) {
             totalBetTokensInExaEsByChoice[2] += _betTokensInExaEs;
+            _arrayIndex = drawBettors.length;
             drawBettors.push(Bettor({
                 bettorAddress: msg.sender,
                 betAmountInExaEs: _betTokensInExaEs
             }));
-            _id = drawBettors.length;
         } else {
             require(false);
         }
@@ -197,17 +205,19 @@ contract Bet {
         // betDeEx contract transfers the tokens to it self
         require(betDeEx.collectBettorTokens(msg.sender, _betTokensInExaEs));
 
-        emit NewBetting(msg.sender, _betTokensInExaEs, _choice, _id);
+        //emit NewBetting(msg.sender, _betTokensInExaEs, _choice, _arrayIndex);
+        betDeEx.emitNewBettingEvent(msg.sender, _choice, _arrayIndex);
     }
 
     function endBet(uint8 _choice) public onlyManager {
 
         // because its required to initialise the pointer with something.
-        Bettor[] storage _winnerBettors = drawBettors;
-        if (_choice == 0) {
-            _winnerBettors = noBettors;
-        } else if (_choice == 1) {
+        Bettor[] storage _winnerBettors = noBettors;
+        if (_choice == 1) {
             _winnerBettors = yesBettors;
+        } else if (_choice == 2) {
+            require(isDrawPossible);
+            _winnerBettors = drawBettors;
         } else if (_choice > 2) {
             require(false);
         }
@@ -983,4 +993,8 @@ contract EraswapToken is PausableEraswap {
       }
       return true;
     }
+
+
+
+
 }
